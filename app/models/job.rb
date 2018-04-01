c39f7ef9641d01c391fc004f6c03f80b7427514e
Hayson <heyson@sina.cn>
2018-04-01 20:13:27 +0800

    order by wage

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,7 +2,8 @@ class Job < ApplicationRecord
   validates :title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
   validates :wage_upper_bound, numericality: { greater_than: 0}
-
+  scope :published, -> { where(is_hidden: false)}
+  scope :recent, -> { order('created_at DESC') }
   def publish!
     self.is_hidden = false
     self.save
